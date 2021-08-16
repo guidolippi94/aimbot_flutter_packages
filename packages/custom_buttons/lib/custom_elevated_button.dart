ca93f159257721a26e12a8093338392c7978d9d9
Guido Lippi <guidolippi94@gmail.com>
2021-08-16 18:24:43 +0200

    buttons can show loading spinner

--- packages/custom_buttons/lib/custom_elevated_button.dart
+++ packages/custom_buttons/lib/custom_elevated_button.dart
@@ -3,34 +3,40 @@ part of 'package:custom_buttons/custom_buttons.dart';
 
 @immutable
 class CustomElevatedButton extends StatelessWidget {
-  const CustomElevatedButton({
-    Key? key,
-    required this.text,
-    required this.callback,
-    this.bgColor,
-    this.fgColor,
-    this.borderRadius = 5.0,
-    this.icon,
-  }) : super(key: key);
+  const CustomElevatedButton(
+      {Key? key,
+      required this.text,
+      required this.callback,
+      this.bgColor,
+      this.fgColor,
+      this.borderRadius = 5.0,
+      this.icon,
+      this.loading = false})
+      : super(key: key);
   final String text;
   final VoidCallback callback;
   final Color? bgColor;
   final Color? fgColor;
   final IconData? icon;
+  final bool loading;
   final double borderRadius;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: callback,
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          if (icon != null) Icon(icon),
-          if (icon != null) const SizedBox(width: 5),
-          Text(text),
-        ],
-      ),
+      child: loading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (icon != null) Icon(icon),
+                if (icon != null) const SizedBox(width: 5),
+                Text(text),
+              ],
+            ),
       style: ElevatedButton.styleFrom(
         primary: bgColor ?? Theme.of(context).primaryColor,
         onPrimary: fgColor ?? Colors.white,
